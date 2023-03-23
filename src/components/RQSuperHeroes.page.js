25087dcae48cc38dc5b4b6341a74642dfe4297db
bookhabit <qnrgoqlt@naver.com>
2023-03-23 17:16:37 +0900

    리액트쿼리로 데이터 패칭해보기

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,7 +1,23 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchSuperHeroes = ()=>{
+    return axios.get("http://localhost:4000/superheroes")
+}
+
 export const RQSuperHeroesPage = () => {
+    const {isLoading,data} = useQuery('super-heroes',fetchSuperHeroes)
+    if(isLoading){
+        return <h2>로딩 중...</h2>
+    }
     return (
-        <div>
-            RQSuper Heroes Page
-        </div>
+        <>
+            <div>
+                <h2>RQSuper Heroes Page</h2>
+                {data?.data.map(hero=>{
+                    return <div key={hero.name}>{hero.name}</div>
+                })}
+            </div>
+        </>
     );
 };
