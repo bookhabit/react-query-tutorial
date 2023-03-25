d09ae9a7871d83e30302f6269fd5cac71c97772d
bookhabit <qnrgoqlt@naver.com>
2023-03-25 16:17:28 +0900

    병렬쿼리

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import { HomePage } from './components/Home.page'
 import { RQSuperHeroesPage } from './components/RQSuperHeroes.page'
 import { SuperHeroesPage } from './components/SuperHeroes.page'
 import { ReactQueryDevtools } from 'react-query/devtools'
+// import { ReactQueryDevtools } from 'react-query-devtools'
 import { TestHeroesPage } from './components/TestHeroes.page'
 import { RQSuperHeroPage } from './components/RQSuperHero.page'
+import { ParallelQueriesPage } from './components/ParallelQueries.page'
+import { DynamicParallelPage } from './components/DynamicParallel.page'
 
 const queryClient = new QueryClient();
 
@@ -27,11 +30,16 @@ function App() {
                 <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
               </li>
               <li>
-                <Link to='/test-heroes'>Test-Heroes</Link>
+                <Link to='/rq-parallel'>rq-parallel</Link>
+              </li>
+              <li>
+                <Link to='/rq-dynamic-parallel'>rq-parallel</Link>
               </li>
             </ul>
           </nav>
           <Routes>
+          <Route path='/rq-dynamic-parallel' element={<DynamicParallelPage heroIds={[1,3]}/>}/>
+            <Route path='/rq-parallel' element={<ParallelQueriesPage/>}/>
             <Route path='/rq-super-heroes/:heroId' element={<RQSuperHeroPage/>}/>
             <Route path='/super-heroes'element={<SuperHeroesPage/>}/>          
             <Route path='/rq-super-heroes' element={<RQSuperHeroesPage />}/>
